601d137d785312a776a475f54cf0f947598b182d
gary <gary.white@lodgit.com>
2021-07-15 13:11:06 +0200

    code

--- Summation.js
+++ Summation.js
@@ -1,1 +1,12 @@
-console.log("hello")
+var summation = function (num) {
+    for (var i = 0; i < num; i++) {
+        console.log("The number is " + num + i);
+        i++;
+        //console.log("hello");
+      }
+    
+  }
+
+
+console.log(summation(2)) // = 3 (1 + 2)
+//console.log(summation(8)) // = 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
